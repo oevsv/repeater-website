f5c906df91cec1f4889de109badaea3de4e21bae
dzlabing <dzlabing@users.noreply.github.com>
2026-06-07 17:35:46 +0000

    Deploying to gh-pages from  @ fdbf7befe68fc8f7eacf9c402700aab33ff097ca 🚀

---
+++ browser/de/chunk-ICMB6H7U.js
@@ -0,0 +1,2 @@
+import{a as e}from"./chunk-IXYUDPMM.js";import{a as r}from"./chunk-C2335RKJ.js";import"./chunk-WG4W3CPC.js";var o=class extends r{decodeBlock(t){return e(new Uint8Array(t)).buffer}};export{o as default};
+/**i18n:434b016ba026a6847e1911da148ad4d59d73f4c3399659538c6e3e321082043c*/
